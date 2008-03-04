8352d60afc0d04e193b912da49f83bf6543aaa77
seth <seth@4da624d7-9686-4b4d-bf4a-96ca807925bb>
2008-03-04 00:00:08 +0000

    remove plugin migrations remove crabgrass rake file
    git-svn-id: http://code.autistici.org/svn/crabgrass/branches/greenchange@996 4da624d7-9686-4b4d-bf4a-96ca807925bb

--- db/migrate/1196894225_crabgrass_document_types_to_version_1195250189.rb
+++
@@ -1,9 +0,0 @@
-class CrabgrassDocumentTypesToVersion1195250189 < ActiveRecord::Migration
-  def self.up
-    Rails.plugins["crabgrass_document_types"].migrate(1195250189)
-  end
-
-  def self.down
-    Rails.plugins["crabgrass_document_types"].migrate(0)
-  end
-end
--- lib/tasks/crabgrass.rake
+++
@@ -1,6 +0,0 @@
-require 'spec/rake/spectask'
-namespace :crabgrass do
-  Spec::Rake::SpecTask.new(:spec) do |t|
-    t.spec_files = FileList["#{RAILS_ROOT}/vendor/plugins/crabgrass*/spec/**/*_spec.rb"]
-  end
-end
