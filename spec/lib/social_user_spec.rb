6ff5d0c4c5a26382f9987a7e05061d1740d344f0
austin <austin@4da624d7-9686-4b4d-bf4a-96ca807925bb>
2008-03-13 18:42:31 +0000

    social user test converted to spec
    git-svn-id: http://code.autistici.org/svn/crabgrass/branches/greenchange@1079 4da624d7-9686-4b4d-bf4a-96ca807925bb

--- spec/lib/social_user_spec.rb
+++ spec/lib/social_user_spec.rb
@@ -1,34 +1,175 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "SocialUser" do
+
+  #fixtures :users, :groups, :memberships, :pages
+
+  before do
+    #TzTime.zone = TimeZone["Pacific Time (US & Canada)"]
+    @user = create_valid_user :login => 'membershiptester'
+    @group = create_valid_group
+  end
+
+  it "should be mixed into user" do
+   @user.is_a?(SocialUser).should be_true
+  end
+
+  describe "in group memberships" do
+    it "should add users when a membership is added" do
+      lambda{ @group.memberships.create :user => @user }.should change( @group.users, :count ).by(1)
+      # @user.memberships.create :group => @group (another valid way to do the same thing)
+      #assert oldcount < @group.users.count, "group should have more users after add user"   
+    end
+    describe "already existing" do 
+      before do
+        @group.memberships.create :user => @user
+      end
+
+      it "finds the user" do
+        @group.users.find(@user.id).should == @user
+      end
+
+      it "finds the group" do
+        @user.groups.find(@group.id).should == @group
+      end
+
+      it "knows its membership status" do
+        @user.should be_member_of(@group)
+      end
+
+      it "notices when membership ends" do
+        @user.groups.delete @group
+        @user.should_not be_member_of(@group)
+      end
+  
+      it "does not allow duplicate memberships" do
+        lambda { @user.memberships.create :group => @group }.should raise_error(AssociationError)
+      end
+    end
+
+    describe "caching membership data" do
+
+      before do
+        @user = create_valid_user :login => 'hermione'
+        @group = create_valid_group :name => 'hogwarts-academy'
+        @group.memberships.create :user => @user
+      end
+
+      it "should have a group ids array" do
+        @user.group_ids.should == [@group.id]
+      end
+
+      it "should include group in all groups array" do
+        @user.all_group_ids.should == [@group.id]
+      end
+
+      it "serialize_as the all_group_ids to a cache" do
+        group2 = create_valid_group :name => 'slytherin'
+        group2.memberships.create :user => @user
+        @user.all_group_id_cache.should == [@group.id, group2.id]
+      end
+
+      it "should update groups array when changes happen" do
+        user = create_valid_user :login => 'hermione'
+        user.groups #performs initial caching
+        group = create_valid_group :name => 'hogwarts-academy'
+        group.memberships.create :user => user
+        user.group_ids.should == [ group.id ]
+      end
+      it "should update the all-groups array when changes happen" do
+        user = create_valid_user :login => 'hermione'
+        user.all_groups #performs initial caching
+        group = create_valid_group :name => 'hogwarts-academy'
+        group.memberships.create :user => user
+        user.all_group_ids.should == [ group.id ]
+      end
+
+    end
+      
+  end
+
+  describe "when having contacts" do
+    before do
+      @a = create_valid_user :login => 'red'
+      @b = create_valid_user :login => 'green'
+    end
+
+    it "starts without contacts" do
+      @a.contacts.should_not include(@b)
+    end
+
+    it "accepts << contacts" do
+      @a.contacts << @b
+      @a.contacts.should include(@b)
+    end
+
+    describe "that already exist" do
+      before do
+        @a.contacts << @b
+      end
+      it "adds contacts to the friend id cache" do
+        @a.reload
+        @a.friend_id_cache.should include(@b.id)
+      end
+      it "notices deletion" do
+        @a.contacts.delete(@b)
+        @a.contacts.should_not include(@b)
+      end
+
+      it "makes an error when duplicating contacts" do
+        lambda { @a.contacts << @b }.should raise_error(AssociationError)
+      end
+    end
+  end
+
+  describe "with committees" do
+    before do
+      @user = create_valid_user :login => 'ron'
+
+      @group = Group.create :name => 'hogwarts-academy'
+      @group.memberships.create :user => @user
+      @committee = Committee.create :name => 'dumbledores-army', :parent => @group
+      @user.reload
+    end
+    it "group member automatically has committee in all_groups collection" do
+      @user.all_group_ids.sort.should == [@group.id, @committee.id].sort
+    end
+  end
+
+
+  describe "when pestering," do
+    before do
+      @user1 = create_valid_user :login => 'pest'
+      @user2 = create_valid_user :login => 'aff'
+    end
+    it "allows mutual pestering by default" do
+      @user1.may_pester?(@user2).should be_true
+      @user2.may_be_pestered_by?(@user1).should be_true
+    end
+    it "pestering can be stopped" do
+      pending "pestering is still globally allowed" 
+    end
+  end
+
+  describe "with pages" do
+    it "should be able to access pending pages" do
+      p = create_valid_page
+      @user.pages << p
+      @user.participations.detect {|up| up.page_id = p.id }.update_attribute :resolved, false
+      @user.pages_pending.should include(p)
+    end
+
+  end
+    
+end
+
+describe "SocialUser" do
   
   before do
     @user = create_valid_user
     
   end
 
-  it "should update groups array when changes happen" do
-    @user.groups
-    group = Group.create :name => 'hogwarts-academy'
-    group.memberships.create :user => @user
-    @user.group_ids.should == [ group.id ]
-  end
-  it "should update the all-groups array when changes happen" do
-    #@user.group_ids
-    #@user.all_group_ids
-    #@user.groups
-    @user.all_groups
-    group = Group.create :name => 'hogwarts-academy'
-    group.memberships.create :user => @user
-    #@user.all_group_ids
-    @user.all_group_ids.should == [ group.id ]
-  end
-
-  it "should be able to access pending pages" do
-    p = create_valid_page
-    @user.pages << p
-    @user.participations.detect {|up| up.page_id = p.id }.update_attribute :resolved, false
-    @user.pages_pending.should include(p)
-  end
 
 end
+
