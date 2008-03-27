8a41156ead62e2bc6e023419b8547105a7c09f5f
Seth Walker <seth@sobriety.local>
2008-03-27 14:43:30 -0700

    discussions get resourceful

--- app/controllers/tool/discussion_controller.rb
+++ app/controllers/tool/discussion_controller.rb
@@ -1,5 +1,21 @@
 class Tool::DiscussionController < Tool::BaseController
 
+  def new
+    @page = Tool::Discussion.new :group_id => params[:group_id]
+  end
+
+  def create
+    @page = Tool::Discussion.new params[:page]
+    @page.created_by = current_user
+    if @page.save
+      @page.tag_with(params[:tag_list]) if params[:tag_list]
+      redirect_to(discussion_url(@page))
+    else
+      message :object => @page
+      render :action => 'new'
+    end
+  end
+
   def show
     @comment_header = ""
   end
--- app/helpers/page_url_helper.rb
+++ app/helpers/page_url_helper.rb
@@ -6,7 +6,7 @@ module PageUrlHelper
     url_helper = case page
     when Tool::Video
       'video_url'
-    when Tool::Blog, Tool::News, Tool::Event, Tool::Message
+    when Tool::Blog, Tool::News, Tool::Event, Tool::Message, Tool::Discussion
       "#{page.class.to_s.demodulize.underscore}_url"
     when Tool::Asset
       'upload_url'
