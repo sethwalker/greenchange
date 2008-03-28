0400bed0420fc007b910c14e221e560adb04d5b2
Seth Walker <seth@sobriety.local>
2008-03-27 20:18:45 -0700

    half assed external video resources, create only

--- app/models/tool/external_video.rb
+++ app/models/tool/external_video.rb
@@ -1,6 +1,9 @@
 class Tool::ExternalVideo < Tool::Video
   class_display_name 'external video'
   class_description 'a video from an external service'
+  belongs_to :data, :class_name => 'ExternalMedia::Base'
+
+  def update_access; end
 
   def icon_path
     data.thumbnail_url
