b15554e2822b313c1e2cea2a825186f16850a7a0
Ammar Ali <ammar@mizmar.(none)>
2008-03-31 15:52:55 -0700

    Removed call to authorized? from login_required as a first step to sanitizing authorization Cleanup events returned list

--- app/controllers/tool/event_controller.rb
+++ app/controllers/tool/event_controller.rb
@@ -224,6 +224,7 @@ class Tool::EventController < Tool::BaseController
   # returns array of events for the current user or group, depending on context
   def list
     @start_date, @end_date = request_dates
+
     if @group
       @events = @group.pages.page_type(:event).occurs_between_dates(
         @start_date.to_s, @end_date.to_s
@@ -238,5 +239,7 @@ class Tool::EventController < Tool::BaseController
         @start_date.to_s, @end_date.to_s
       ).find(:all, :order => "pages.starts_at ASC")
     end
+
+    @events.uniq!
   end
 end
