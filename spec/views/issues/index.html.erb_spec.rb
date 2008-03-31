04d6c740b3574a1656b26e49fe0c1c55f19fa12e
David Taylor <David@David-Taylor.yourdomain.com>
2008-03-30 18:42:52 -0700

    changed scpec not to fail

--- spec/views/issues/index.html.erb_spec.rb
+++ spec/views/issues/index.html.erb_spec.rb
@@ -1,12 +1,12 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe "/issues/index" do
-  before(:each) do
-    render 'issues/index'
-  end
+#describe "/issues/index" do
+#  before(:each) do
+#    render 'issues/index'
+#  end
   
   #Delete this example and add some real ones or delete this file
-  it "should tell you where to find the file" do
-    response.should have_tag('p', /Find me in app\/views\/issues\/index/)
-  end
-end
+#  it "should tell you where to find the file" do
+#    response.should have_tag('p', /Find me in app\/views\/issues\/index/)
+#  end
+#end
