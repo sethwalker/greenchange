67ac18fbdbdd212127c756470966837135bde076
Austin Putman <austin@emmanuel.local>
2008-03-30 23:08:52 -0700

    images_url straggler

--- db/schema.rb
+++ db/schema.rb
@@ -91,9 +91,9 @@ ActiveRecord::Schema.define(:version => 1206835544) do
     t.integer  "created_by"
     t.integer  "position"
     t.string   "collectable_type"
+    t.string   "permission"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "permission"
   end
 
   create_table "collections", :force => true do |t|
@@ -355,11 +355,6 @@ ActiveRecord::Schema.define(:version => 1206835544) do
   end
 
   add_index "phone_numbers", ["profile_id"], :name => "phone_numbers_profile_id_index"
-
-  create_table "plugin_schema_info", :force => true do |t|
-    t.datetime "created_at"
-    t.string   "plugin_name"
-  end
 
   create_table "polls", :force => true do |t|
     t.string "type"
