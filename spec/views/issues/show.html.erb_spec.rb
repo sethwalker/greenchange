4cb9784b41e5980d73514ba1575c98e947758cce
Austin Putman <austin@emmanuel.local>
2008-03-30 17:37:11 -0700

    more issues stuff, normalized some views

--- spec/views/issues/show.html.erb_spec.rb
+++ spec/views/issues/show.html.erb_spec.rb
@@ -6,7 +6,7 @@ describe "/issues/show" do
   end
   
   #Delete this example and add some real ones or delete this file
-  it "should tell you where to find the file" do
-    response.should have_tag('p', /Find me in app\/views\/issues\/show/)
-  end
+  #it "should tell you where to find the file" do
+    #response.should have_tag('p', /Find me in app\/views\/issues\/show/)
+  #end
 end
