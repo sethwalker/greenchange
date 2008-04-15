8d3dff97c1637364fed8e1cfe55d97388cc3bb4c
Austin Putman <austin@emmanuel.local>
2008-04-14 22:50:11 -0700

    inbox messages now work okay

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,3 +1,8 @@
 class Invitation < Message
   belongs_to :invitable, :polymorphic => true
+
+  def after_accepted
+    if self.invitable < Event
+      event.attendees.create :user => recipient
+  end
 end
