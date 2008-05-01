821e1db9ad42f05f36b5b83a1e0be1e588a675c1
Austin Putman <austin@emmanuel.local>
2008-05-01 14:53:59 -0700

    fixed saving bug for web resources, data now saves to all profiles

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/../spec_helper.rb'
 describe "Profile::Profile" do
 
   before do
-    @user = User.new( :login => 'testo' , :password => 'test', :password_confirmation => 'test' )
+    @user = User.new( :login => 'testo' , :password => 'test', :password_confirmation => 'test', :email => 'jk@ju.co' )
     @profile = Profile.new(:friend => true, :entity => @user )
   end
 
@@ -31,6 +31,25 @@ describe "Profile::Profile" do
   it "validates first_name when built by the user" do
     profile = @user.build_private_profile :friend => true, :entity => @user 
     profile.should have_at_least(1).errors_on(:last_name)
+  end
+
+  describe "saving a public version" do
+    before do
+      @profile = @user.build_private_profile :friend => true, :entity => @user, :first_name => 'j', :last_name => 'k'
+      @wr = @profile.web_resources.build :web_resource_type => "flickr", :web_resource => "badassbetty"
+    end
+  
+    it "should copy the web resource to the public version" do
+      @user.save
+      user = User.find @user.id
+      user.public_profile.web_resources.first.web_resource.should == 'badassbetty'
+    end
+    it "should keep the web resource on the private, editable version" do
+      @user.save
+      profile = Profile.find @profile.id
+      profile.web_resources.first.web_resource.should == 'badassbetty'
+    end
+    
   end
 end
 
