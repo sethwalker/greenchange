b11375c9cd88ea002841a0ed7082455d8fdbcebc
Austin Putman <austin@emmanuel.local>
2008-05-07 22:37:42 -0700

    ditched outdated specs for requests controller

--- spec/controllers/requests_controller_spec.rb
+++ spec/controllers/requests_controller_spec.rb
@@ -4,31 +4,4 @@ describe RequestsController do
   before do
     login_valid_user
   end
-  describe "contacts" do
-    it "should paginate the collection" do
-      get :contacts
-      assigns[:requests].should be_a_kind_of(WillPaginate::Collection)
-    end
-  end
-  describe "memberships" do
-    it "should paginate the collection" do
-      get :memberships, :path => ['created_at', 'descending']
-      assigns[:requests].should be_a_kind_of(WillPaginate::Collection)
-    end
-  end
-  describe "mine" do
-    it "should paginate the collection" do
-      get :mine, :path => ['created_at', 'descending']
-      assigns[:requests].should be_a_kind_of(WillPaginate::Collection)
-    end
-  end
-  describe "index" do
-    it "should paginate the collections" do
-      get :index
-      assigns[:my_contact_requests_sent].should_not be_nil
-      assigns[:my_contact_requests_received].should_not be_nil
-      assigns[:my_membership_requests].should_not be_nil
-    end
-  end
-
 end
