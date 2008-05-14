ad4cdadca4a48006cddea936e565f1af260ca916
Seth Walker <seth@sobriety.local>
2008-05-14 14:47:19 -0700

    fix migration for sqlite

--- db/migrate/1208908928_contacts_has_id.rb
+++ db/migrate/1208908928_contacts_has_id.rb
@@ -1,8 +1,20 @@
 class ContactsHasId < ActiveRecord::Migration
   def self.up
-    add_column :contacts, :id, :primary_key, :auto_increment => true
-    # re-saving each item creates reciprocal relationships where needed
-    Contact.find( :all ).each(&:save)
+    if ActiveRecord::Base.connection.adapter_name == 'SQLite'
+      contacts = Contact.find(:all)
+      drop_table :contacts
+      create_table :contacts, :force => true do |t|
+        t.integer "user_id"
+        t.integer "contact_id"
+      end
+      add_index "contacts", ["contact_id", "user_id"], :name => "index_contacts"
+      Contact.reset_column_information
+      contacts.each(&:save)
+    else
+      add_column :contacts, :id, :primary_key, :auto_increment => true
+      # re-saving each item creates reciprocal relationships where needed
+      Contact.find( :all ).each(&:save)
+    end
   end
 
   def self.down
