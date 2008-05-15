9f002526df29a6019d71fd860cc43a325dd8c9c0
Austin Putman <austin@emmanuel.local>
2008-05-15 12:32:51 -0700

    add migration for contact requests

--- db/migrate/1210034568_join_request_messages.rb
+++ db/migrate/1210034568_join_request_messages.rb
@@ -6,11 +6,15 @@ class JoinRequestMessages < ActiveRecord::Migration
     reqs.each do |req|
       JoinRequest.create :state => req.state, :sender => req.user, :body => req.message, :approved_by_id => req.approved_by_id, :requestable => req.group
     end
+    reqs = ContactRequest.find :all
+    reqs.each do |req|
+      Invitation.create :state => req.state, :sender => req.user, :body => req.message, :invitable => req.user, :recipient_id => req.contact_id
+    end
   end
 
   def self.down
+    raise ActiveRecord::IrreversableMigration
     add_column :messages, :group_id, :integer
     remove_column :messages, :approved_by_id
-    raise ActiveRecord::IrreversableMigration
   end
 end
