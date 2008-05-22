806e127b9a2967e49784e61fdda7f54ace481ef1
Austin Putman <austin@emmanuel.local>
2008-05-22 13:37:24 -0700

    tag show redirects to tag_pages

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,8 @@
 class TagsController < ApplicationController
   def show
     @tag = Tag.find_by_name(params[:id])
+    raise ActiveRecord::RecordNotFound unless @tag
+    redirect_to tag_pages_path(@tag)
     @pages = @tag.pages.allowed(current_user, :view).find(:all, :order => 'pages.updated_at DESC', :limit => 20)
   end
 
