975dd793f71218ca7f8bba47bd797a30162d66b9
Austin Putman <austin@radicaldesigns.org>
2008-06-08 14:12:18 -0700

    jquery tabs working

--- public/javascripts/tabs.js
+++ public/javascripts/tabs.js
@@ -1,3 +1,6 @@
+jQ$.fn.sort = function() { 
+  return this.pushStack( jQuery.makeArray( [].sort.apply( this, arguments )) ); 
+};
 Crabgrass.Tabs =  function() {
       return {
         create: function() {
@@ -6,99 +9,78 @@ Crabgrass.Tabs =  function() {
             segments : new Array( ),
             segment_count: 0,
             add: function( element, trigger  ) {
-                self.segments[ self.segment_count ] = $( element );
-                self.triggers[ self.segment_count ] = $( trigger );
-                Event.observe( $( trigger ), 'click', function( ){ self.show( $( element ) ); }, self ) ;
+                self.segments[ self.segment_count ] = element;
+                self.triggers[ self.segment_count ] = trigger;
+                jQ$(trigger).click( function(ev){ self.show(element); } ) ;
                 ++self.segment_count;
                 },
             show: function( element ) {
-                var key = self.segments.indexOf( element );
+                var key = jQ$(self.segments).index( element );
                 var trigger =  self.triggers[key];
-                var to_check = self.segments.without(self.segments[key]);
-                var items_to_hide = to_check.select( function( item ) { return item.visible(); } )
-                
-                if ( !( self.segments[key].visible() && items_to_hide.all( function(item){ return !item.visible(); } ) )) { 
-                  self.segments.each( function(e) { e.removeClassName( 'active'); } );
-                  jQuery(items_to_hide).hide('slide', { duration: 150, direction: 'left', callback: function(){ 
-                    jQuery(self.segments[key]).show('slide', { duration: 150, direction: 'right', callback: function() { 
-                        self.segments[key].addClassName( 'active');
-                      } }) 
-                    }} );
-                }
-                self.triggers.each( function( item ) { item.removeClassName( 'active');});
-                trigger.addClassName( 'active');
+                jQ$(self.segments).removeClass( 'active'); 
+                console.log( self.segments[key].inspect() );
+                console.log(jQ$(self.segments).not( self.segments[key]).is(":visible").length);
+                if( jQ$(self.segments).not( self.segments[key]).is(":visible")) {
+                  jQ$(self.segments).not( self.segments[key]).filter( ":visible" ).hide('slide', 
+                    { duration: 150, direction: 'left',   
+                      callback: function(){ 
+                          jQ$(self.segments[key]).show('slide', 
+                            { duration: 150, direction: 'right', 
+                              callback: function() { jQ$(self.segments[key]).addClass( 'active'); } }) 
+                        }} );
+                } 
+                jQ$(self.triggers).removeClass( 'active');
+                jQ$(trigger).addClass( 'active');
               },
-            collapse: function( ) {
-                self.segments.each( function( item ) { if (item.visible()) new Effect.SwitchOff( item );});
-              },
-            expand: function( ) {
-                self.segments.each( function( item ) { if (!item.visible()) new Effect.Appear( item );});
-              }
           };
           return self;
         },
+        timestamp_sort: function(entry_a, entry_b){ 
+          var time_a = jQ$('.friendly_date', entry_a).length;
+          var time_b = jQ$('.friendly_date', entry_b).length;
+          if(!(time_a||time_b)) return 0;
+          if(!time_a) return 1;
+          if(!time_b) return -1;
+          var time_a = jQ$(".friendly_date", entry_a)[0].readAttribute('title');
+          var time_b = jQ$(".friendly_date", entry_b)[0].readAttribute('title');
+          return ( time_a > time_b ) ? -1 : ( ( time_a < time_b ) ?  1 : 0 );
+        },
         initialize_tab_blocks: function( )  {
-          $$("ul.tab-block").each( function(ul) { 
+          jQ$("ul.tab-block").each( function() { 
+            var ul = this;
             var ul_tabs = Crabgrass.Tabs.create();
 
+            //disable inactive tabs
+            jQ$('.tab-title + .tab-content:not(:has(li))', ul).prev().addClass('disabled');
+
             //copy all list entries to the "all" list
-            var all_list = document.createElement('ul');
-            Element.extend(all_list);
-            all_list.addClassName('tab-content list active');
-            var sorted_entries = ul.select('.list-item').sortBy( function(entry){ 
-              var timestamp = entry.down(".friendly_date");
-              if(timestamp==null) return false;
-              return timestamp.readAttribute('title');
-              }).reverse();
-            sorted_entries.each( 
-                function(entry) { 
-                  all_list.appendChild( entry.cloneNode(true) ); 
-                } 
-            );
+            var all_list = jQ$('.list-item', ul).clone().sort( Crabgrass.Tabs.timestamp_sort );
 
             // add the "all" block to the existing ul
-            ul.select('.tab-content').invoke('hide');
-            var all_block = document.createElement('li');
-            Element.extend( all_block );
-            all_block.insert( all_list );
-            ul.insert( { top: all_block } );
+            jQ$('.tab-content', ul).hide();
+            all_list.prependTo( ul).wrapAll('<li><ul class="tab-content list active"></ul></li>').parent().before('<div class="tab-title">All</div>');
+
+            var triggers = jQ$('.tab-title', jQ$(ul).children()).not('.disabled');
+            var segments = triggers.next('.tab-content');
 
             //create tab-titlebar
-            var titlebar = Element.extend( document.createElement('div'));
-            titlebar.addClassName('tab-titlebar');
-            ul.select('.tab-title').each( function(title) { //add tab-titles
-                if( title.next('.tab-content') && title.next('.tab-content').down('li')) {
-                  ul_tabs.add( title.next('.tab-content'), title );
-                } else {
-                  title.addClassName('disabled');
-                }
-                titlebar.appendChild( title.remove()); } 
-            );
-            ul.insert( { top: titlebar } );
-
-            //add all tab-title
-            var all_title = Element.extend( document.createElement('div') );
-            all_title.addClassName('tab-title');
-            all_title.insert('All');
-            titlebar.insert( { top: all_title } );
+            var titlebar = jQ$('.tab-title', ul).remove().prependTo(jQ$(ul)).wrapAll('<div class="tab-titlebar"></div>').parent();
 
             //move toolbar into titlebar
-            // watch out -- this will move the toolbar for list-item as well
-            ul.select('.toolbar.tabs-menu').each( function(toolbar) { titlebar.insert( {bottom: toolbar.remove() } ) } );
+            jQ$('.toolbar.tabs-menu', ul).remove().appendTo( jQ$('tab-titlebar', ul));
   
+            //var triggers = jQ$('.tab-title:not[.disabled]', ul);
+            triggers.each( function() { 
+                ul_tabs.add( segments[triggers.index(this)], this ); 
+              } );
+
             //setup 'all' list as a tab
-            ul_tabs.add( all_list, all_title );
-            ul.addClassName('active');
-            ul_tabs.show( all_list );
+            jQ$(ul).addClass('active');
+            ul_tabs.show( segments[0] );
 
             //add alternating color classes
-            //ul.select('.tab-content').each( function( tab_contents ) { 
-            new Array( all_list ).each( function( tab_contents ) { 
-              tab_contents.select('.list-item:nth-child(odd)').each( 
-                function( list_item ) { list_item.addClassName( "shade-odd" ); list_item.removeClassName('shade-even');  } );
-              tab_contents.select('.list-item:nth-child(even)').each( 
-                function( list_item ) { list_item.addClassName( "shade-even" ); list_item.removeClassName('shade-odd'); } );
-            } );
+            jQ$('.list-item:even', all_list).addClass('shade-even').removeClass('shade-odd');
+            jQ$('.list-item:odd', all_list).addClass('shade-odd').removeClass('shade-even');
           } );
         }
       }
