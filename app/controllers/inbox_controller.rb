1756b4467693ced690d140708670988208a3a70f
Austin Putman <austin@radicaldesigns.org>
2008-06-30 17:25:21 -0700

    testing empty tabs code

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -5,67 +5,15 @@ class InboxController < ApplicationController
   def show
     @messages = @me.messages_received.pending.paginate(:all, :order => "updated_at DESC", :page => params[:page] )
     @join_requests = JoinRequest.to(@me).pending.paginate :all, :order => "updated_at DESC", :page => params[:page]
-    @items = ( @messages + @join_requests ).sort { |item, item2 | item2.updated_at <=> item.updated_at }.compact
+    @items_received = ( @messages + @join_requests ).sort { |item, item2 | item2.updated_at <=> item.updated_at }.compact
 
-    respond_to do |format|
-      format.html {}
-      format.rss do
-        options = {
-          :title => 'Crabgrass Inbox', 
-          :link => me_inbox_path,
-          :image => avatar_url( @me.avatar || 0, :size => 'standard' ),
-          :items => @messages
-          }
-          render :partial => 'pages/rss', :locals => options
-      end
-        
-    end
+    @messages = @me.messages_received.pending.paginate(:all, :order => "updated_at DESC", :page => params[:page] )
+    @join_requests = @me.join_requests.paginate :all, :order => "updated_at DESC", :page => params[:page]
+    @items_sent= ( @messages + @join_requests ).sort { |item, item2 | item2.updated_at <=> item.updated_at }.compact
 
+    @items_deleted = []
   end
 
-  def search
-    if path.first == 'unread'
-      @pages = current_user.pages_unread.paginate(:all, :page => params[:section], :order => "pages.updated_at DESC")#, :include => :user_participations)
-    elsif path.first == 'pending'
-      @pages = current_user.pages_pending.paginate(:all, :page => params[:section], :order => "pages.updated_at DESC")
-    elsif path.first == 'starred'
-      @pages = current_user.pages_starred.paginate(:all, :page => params[:section], :order => "pages.updated_at DESC")#, :include => :user_participations)
-    elsif path.first == 'vital'
-      @pages = current_user.vital_pages.paginate(:all, :page => params[:section], :order => "pages.updated_at DESC")#, :include => :user_participations)
-    elsif path.first == 'type'
-      @pages = current_user.pages.page_type(path[1]).paginate(:all, :page => params[:section], :order => "pages.updated_at DESC")
-    else
-      @pages = current_user.pages.paginate(:all, :page => params[:section], :order => "pages.updated_at DESC")#, :include => :user_participations)
-    end
-  end 
-
-# post required
-  # action for mass updates
-  def update
-    if params[:remove] 
-      remove
-    else
-      ## add more actions here later
-    end
-  end
-
-  # post required
-  def remove
-    to_remove = params[:page_checked]
-    if to_remove
-      to_remove.each do |page_id, do_it|
-        if do_it == 'checked' and page_id
-          page = Page.find_by_id(page_id)
-          if page
-            upart = page.participation_for_user(@user)
-            upart.destroy
-          end
-        end
-      end
-    end
-    redirect_to url_for(:controller => 'inbox', :action => 'index', :path => params[:path]) 
-  end
-  
   protected
   
   append_before_filter :fetch_user
