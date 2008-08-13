25cc3aba81d2703e08c4eddb208f8ac412a41406
Radical Designs <dev@radicaldesigns.org>
2008-08-13 11:42:07 -0700

    reposts are indexed

--- app/models/tool/repost.rb
+++ app/models/tool/repost.rb
@@ -1,4 +1,11 @@
 class Tool::Repost < Tool::Blog
+  indexes.clear
+  define_index do
+    indexes title
+    indexes [summary, data.body], :as => 'content'
+    has :public
+    set_property :delta => true
+  end
   belongs_to :data, :class_name => '::Repost', :foreign_key => "data_id"
   def allows?(user, action)
     action = action.to_sym
