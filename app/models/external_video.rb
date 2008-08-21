41a9852f180ef87f35808e2c6684fb18b9a1be4b
Austin Putman <austin@radicaldesigns.org>
2008-08-21 14:50:01 -0700

    blip tv supported by external video

--- app/models/external_video.rb
+++ app/models/external_video.rb
@@ -1,12 +1,14 @@
 class ExternalVideo < ExternalMedia
   YOUTUBE_TEMPLATE = %Q[<object width="%2$d" height="%3$d"><param name="movie" value="http://www.youtube.com/v/%1$s"></param><param name="wmode" value="transparent"></param><embed src="http://www.youtube.com/v/%1$s" type="application/x-shockwave-flash" wmode="transparent" width="%2$d" height="%3$d"></embed></object>]
   GOOGLE_VIDEO_TEMPLATE = %Q[<embed id="VideoPlayback" style="width:%2$dpx;height:%3$dpx" allowFullScreen="true" src="http://video.google.com/googleplayer.swf?docid=%1$s&hl=en&fs=true" type="application/x-shockwave-flash"> </embed>]
+  BLIPTV_TEMPLATE = %Q[<embed src="http://blip.tv/play/%1$s" type="application/x-shockwave-flash" width="%2$d" height="%3$d" allowscriptaccess="always" allowfullscreen="true"></embed>]
+
 
   before_validation { @service_name = nil }
   validate :supported
 
   def supported
-    errors.add(:media_embed, "is not supported (currently only youtube and google video)") unless service_name
+    errors.add(:media_embed, "is not supported (currently only youtube, google video, and blip.tv)") unless service_name
   end
 
   def service_name
@@ -15,6 +17,8 @@ class ExternalVideo < ExternalMedia
       :youtube
     when /video.google.com/
       :google_video
+    when /blip.tv/
+      :bliptv
     end
   end
 
@@ -35,6 +39,8 @@ class ExternalVideo < ExternalMedia
       media_embed[/youtube.com\/v\/([\w-]+)/, 1]
     when :google_video
       media_embed[/video.google.com\/googleplayer.swf\?docid=([\w-]+)/, 1]
+    when :bliptv
+      media_embed[/blip.tv\/play\/([\w-]+)/, 1]
     end
   end
 
@@ -52,6 +58,8 @@ class ExternalVideo < ExternalMedia
       "425"
     when :google_video
       "400"
+    when :bliptv
+      "480"
     end
   end
 
@@ -61,6 +69,8 @@ class ExternalVideo < ExternalMedia
       "355"
     when :google_video
       "326"
+    when :bliptv
+      "300"
     end
   end
 
@@ -70,6 +80,8 @@ class ExternalVideo < ExternalMedia
       YOUTUBE_TEMPLATE % [media_key, width, height]
     when :google_video
       GOOGLE_VIDEO_TEMPLATE % [media_key, width, height]
+    when :bliptv
+      BLIPTV_TEMPLATE % [media_key, width, height]
     end
   end
 end
